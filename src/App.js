42edad556d7cb2711276010a5040c38d52ea4975
zmhp <manashahp1995@gmail.com>
2022-08-24 01:06:34 +0530

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+
+        <Navbar />
+
+        ccccccccccc
 
         <Footer />
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <div className="navbar">
+
+            <div className="logo">OX Blog</div>
+
+            <button>New Blog</button>
+
+        </div>
+    );
+}
+ 
+export default Navbar;

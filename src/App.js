7df350b13926267b9b3c5ca53596e7633a85de6e
zmhp <manashahp1995@gmail.com>
2022-08-24 02:03:26 +0530

    Add home and other main components to app component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import './App.css';
 import Footer from './components/Footer';
+import Home from './components/Home';
 import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
+	return (
+		<div className="App">
 
-        <Navbar />
+			<div className="content">
 
-        ccccccccccc
+				<Navbar />
 
-        <Footer />
+				<Home />
+			
+			</div>
 
-    </div>
-  );
+			<Footer />
+
+		</div>
+	);
 }
 
 export default App;

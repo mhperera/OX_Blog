143f49c486917cd049b5a24d9ea911de1f319c85
zmhp <manashahp1995@gmail.com>
2022-08-24 00:50:46 +0530

    Footer Component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+
+            <p>Copyright 2022 OrendaX | All rights reserve</p>
+
+        </div>
+    );
+}
+ 
+export default Footer;

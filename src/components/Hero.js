9b0e94f68ab9be2dde138dd5185e7bc216198bd6
zmhp <manashahp1995@gmail.com>
2022-08-24 10:35:22 +0530

    Added Hero component to the home component

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+const Hero = () => {
+    return (
+        <div className="hero">
+
+            
+
+        </div>
+    );
+}
+ 
+export default Hero;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import useFetch from "../custom_hooks/useFetch";
 import BlogPreview from "./BlogPreview";
+import Hero from "./Hero";
 
 const Home = () => {
 
@@ -8,21 +9,23 @@ const Home = () => {
     return (
         <div className="home">
 
-            <h1>All Blog Posts</h1>
+            <Hero />
 
-            { blogs && (
-            
-                    blogs.map((blog) => (
-                        <BlogPreview 
-                            title = { blog.title }  
-                            author = { blog.author }  
-                            body = { blog.body.substring(0, 150) } 
-                            key = { blog.id } 
-                        />
-                    ))
-                    
-                )
-            }  
+            <div className="container row">
+                { blogs && (
+                
+                        blogs.map((blog) => (
+                            <BlogPreview 
+                                title = { blog.title }  
+                                author = { blog.author }  
+                                body = { blog.body.substring(0, 150)+' ...' } 
+                                key = { blog.id } 
+                            />
+                        ))
+                        
+                    )
+                } 
+            </div>
 
         </div>
     );

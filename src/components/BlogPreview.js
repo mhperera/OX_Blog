d9e8203dad21c00b4ded925927349f1ab8e39a9d
zmhp <manashahp1995@gmail.com>
2022-08-24 10:35:40 +0530

    Added sample image for the blog preview

--- src/components/BlogPreview.js
+++ src/components/BlogPreview.js
@@ -1,10 +1,16 @@
 const BlogPreview = ({ title, author, body }) => {
 
     return (
-        <div className="blog-preview">
-            <p className="blog-title">{ title }</p>
-            <p className="blog-author">{ author }</p>
-            <p className="blog-body">{ body +' ...' }</p>
+        <div className="col-sm-4 p-0">
+            <div className="card">
+                <img src="./assets/images/blog_images/sample.jpg" class="card-img-top" alt="..."></img>
+                <div className="blog-preview card-body">
+                    <p className="card-title blog-title">{ title }</p>
+                    <p className="card-text blog-author">{ author }</p>
+                    <p className="card-text blog-body">{ body }</p>
+                    <button className="float-end">Read More</button>
+                </div>
+            </div>
         </div>
     );
     

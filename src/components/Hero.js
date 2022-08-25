c327e8788e4c105a2bd5a29caab5db03f2352d6f
zmhp <manashahp1995@gmail.com>
2022-08-25 22:56:54 +0530

    Edited Text in Home Hero Section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
     return (
         <div className="hero">
 
-            <h1>Orendax Blog</h1>
+            <h1>OrendaX Blog</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita id nesciunt odio. Fugit dolores doloribus modi rem dolorum ut tenetur exercitationem ipsa vel? Iure laboriosam perspiciatis voluptates dolorum ullam rem sequi, inventore quae cumque </p>
 
             <Link to='/blogs' >

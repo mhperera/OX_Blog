47d4b8d770412703111d70e84d2a481862c55c1e
zmhp <manashahp1995@gmail.com>
2022-08-25 22:55:47 +0530

    Blog Preview Edited

--- src/components/BlogPreview.js
+++ src/components/BlogPreview.js
@@ -1,14 +1,18 @@
-const BlogPreview = ({ title, author, body }) => {
+import { Link } from "react-router-dom";
+
+const BlogPreview = ({ title, author, body, id }) => {
 
     return (
         <div className="col-sm-4 p-0">
             <div className="card">
-                <img src="./assets/images/blog_images/sample.jpg" className="card-img-top" alt="..."></img>
+                {/* <img src="./assets/images/blog_images/sample.jpg" className="card-img-top" alt="..."></img> */}
                 <div className="blog-preview card-body">
                     <p className="card-title blog-title">{ title }</p>
                     <p className="card-text blog-author">{ author }</p>
                     <p className="card-text blog-body">{ body }</p>
-                    <button className="float-end">Read More</button>
+                    <Link to={ '/blogs/' + id }>
+                        <button className="float-end">Read More</button>
+                    </Link>
                 </div>
             </div>
         </div>

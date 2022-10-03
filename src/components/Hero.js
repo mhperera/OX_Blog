9678ba393994728122298c511a0890d917e160c2
zmhp <manashahp1995@gmail.com>
2022-10-03 22:18:02 +0530

    Adjust CSS

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
     return (
-        <div className="hero">
+        <div className="hero" style={{ backgroundImage: "url(/assets/images/Pink3.png)" }}>
 
             <h1>OrendaX Blog</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita id nesciunt odio. Fugit dolores doloribus modi rem dolorum ut tenetur exercitationem ipsa vel? Iure laboriosam perspiciatis voluptates dolorum ullam rem sequi, inventore quae cumque </p>
